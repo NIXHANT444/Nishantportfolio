21862b4c05a99ac20eff394e59e6167d4f76538c
NIXHANT444 <PATELNISHANT2006@GMAIL.COM>
2025-02-07 22:11:49 +0530

    Update start_render.sh

--- start_render.sh
+++ start_render.sh
@@ -1,27 +1,3 @@
-#!/bin/bash
-echo "🚀 Starting Render deployment setup..."
-
-# Client setup
-cd client
-echo "🔧 Installing client dependencies..."
-npm install next@latest
-rm -rf .next
-rm -rf node_modules
-npm ls react react-dom
-npm install
-rm -rf .next
-npm run build
-echo "✅ Client dependencies installed."
-
-cd ..
-
-# Server setup
-cd server
-echo "🔧 Installing server dependencies..."
-npm install
-echo "✅ Server dependencies installed."
-echo "🔄 Starting the server..."
-npm ls react react-dom
 npm start
 echo "✅ Server started."
 

b13a1ce7916a991cb13b0dc7361ba4bb5eae3192
NIXHANT444 <PATELNISHANT2006@GMAIL.COM>
2025-02-07 21:56:23 +0530

    Update start_render.sh

--- start_render.sh
+++ start_render.sh
@@ -4,8 +4,13 @@ echo "🚀 Starting Render deployment setup..."
 # Client setup
 cd client
 echo "🔧 Installing client dependencies..."
-npm install tailwindcss postcss autoprefixer
+npm install next@latest
+rm -rf .next
+rm -rf node_modules
+npm ls react react-dom
 npm install
+rm -rf .next
+npm run build
 echo "✅ Client dependencies installed."
 
 cd ..
@@ -16,7 +21,7 @@ echo "🔧 Installing server dependencies..."
 npm install
 echo "✅ Server dependencies installed."
 echo "🔄 Starting the server..."
-node server.js &
+npm ls react react-dom
+npm start
 echo "✅ Server started."
-
 
